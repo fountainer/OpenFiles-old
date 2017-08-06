884c6d2b7644de0d2ecc04f8a1609e7a7d5a8a0a
fountainer <fountainer@163.com>
2017-08-06 19:37:27 +0800

    initial commit

---
+++ OpenFiles.py
@@ -0,0 +1,31 @@
+import sublime, sublime_plugin
+import os
+from os.path import join
+
+class OpenFilesCommand(sublime_plugin.TextCommand):
+    def run(self, edit, path = None):
+        files, folders, path = self.get_files_folders(path)
+        entries = [folder + "/" for folder in folders] + files
+        if not entries:
+            sublime.message_dialog("Empty folder!")
+            return 
+        window = self.view.window()
+
+        def on_done(index):
+            if index >= len(folders):
+                window.open_file(join(path, entries[index]))
+            elif (index >= 0):
+                sub_path = join(path, entries[index])
+                window.run_command("open_files", {"path": sub_path})
+            else:
+                pass
+
+        window.show_quick_panel(entries, on_done)
+
+    def get_files_folders(self, path = None):
+        if path is None:
+            path = os.path.dirname(self.view.file_name())
+        entries = os.listdir(path)
+        files = [file for file in entries if os.path.isfile(join(path, file))]
+        folders = list(set(entries) - set(files))
+        return((files, folders, path))
---
+++ README.md
@@ -0,0 +1,4 @@
+### Open Files in the current folder
+
+Press `ctrl + shift + o` to display the files and folders in the current folder
+in the quick panel. Select a file and open it or select and display a subdirectory.
